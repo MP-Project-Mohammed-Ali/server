26230e5967b2bf7ed242b39bd154b354e6f5e78b
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-12-20 18:41:07 +0300

    LoginRoutesV1Done

--- routers/routes/user.js
+++ routers/routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const {register} = require("./../controllers/user");
+const {register,login} = require("./../controllers/user");
 const userRouter = express.Router();
 
 userRouter.post("/singup", register);
-
+userRouter.post("/login",login)
 
 module.exports = userRouter;
